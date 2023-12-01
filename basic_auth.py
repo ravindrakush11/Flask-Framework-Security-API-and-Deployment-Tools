08b9ccd801654fa3a9f0d6c4dbe75fa32078bf5e
Ravindra Singh Kushwaha <94731464+ravindrakush11@users.noreply.github.com>
2023-12-01 12:45:53 +0530

    Uses HTTP Basic Authentication in Flask App

--- basic_auth.py
+++ basic_auth.py
@@ -1,25 +1,28 @@
-from flask import Flask
-from flask_httpauth import HTTPBasicAuth
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask, jsonify, request, make_response
+from functools import wraps
+
+# https://www.youtube.com/watch?v=VW8qJxy4XcQ&ab_channel=PrettyPrinted
 
 app = Flask(__name__)
-auth = HTTPBasicAuth()
 
-users = {
-    "john": generate_password_hash("hello"),
-    "susan": generate_password_hash("bye")
-}
+def auth_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if auth and  auth.username == 'username' and auth.password == 'password':
+            return f(*args, **kwargs)
+        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm = "Login Required"'})
+    return decorated
 
-@auth.verify_password
-def verify_password(username, password):
-    if username in users and \
-            check_password_hash(users.get(username), password):
-        return username
+@app.route('/page')
+@auth_required
+def page():
+    return '<h1> You are on the page!</h1>'
 
-@app.route('/', )
-@auth.login_required
-def index():
-    return "Hello, %s!" % auth.current_user()
+@app.route('/other page')
+def other_page():
+    return 'You are on the other page'
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port='5003')
