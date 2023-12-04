80291f5843276e2655683aabec4ef013a3d38047
Ravindra Singh Kushwaha <94731464+ravindrakush11@users.noreply.github.com>
2023-12-04 12:19:10 +0530

    Updated the limiter section
    Added some new methods in the limiter sections

--- flask limiter.py
+++ flask limiter.py
@@ -1,14 +1,25 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
 # https://flask-limiter.readthedocs.io/en/stable/index.html
+# https://www.youtube.com/watch?v=vQleDvTM5xA&list=PLXmMXHVSvS-CoYS177-UvMAQYRfL3fBtX&index=52&ab_channel=PrettyPrinted
 
 app = Flask(__name__)
-limiter = Limiter(key_func=get_remote_address,app=app,  default_limits=["10 per day", "1 per hour"], )
+
+limiter = Limiter(
+    app=app,
+    key_func=get_remote_address,
+    default_limits=["100 per day"],
+    application_limits_cost=lambda: 2,
+    fail_on_first_breach=True,
+    on_breach=lambda _: jsonify({'message': 'Rate limit breached!'}),
+    request_identifier=lambda: get_remote_address(),   #  create a custom identifier for a request
+    default_limits_cost=1
+)
 
 @app.route('/api')
-@limiter.limit('3 per day', override_defaults=False, error_message = "You have consumed all the tokens pls wait for some time", )
+@limiter.limit('5 per minute')
 def api():
     return 'This is the response from API'
 
