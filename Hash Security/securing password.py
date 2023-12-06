8e135ba4604e00534d0b81b5cbfad49e2db4a434
Ravindra Singh Kushwaha <94731464+ravindrakush11@users.noreply.github.com>
2023-12-06 13:01:47 +0530

    Incremented the security
    added fs_uniquifier, migration, etc. From this file we can register the data to the database, but when try to login it throws an error, password is invalid.
    The issue needs to be addressed flask application file.

--- Hash Security/securing password.py
+++ Hash Security/securing password.py
@@ -1,16 +1,22 @@
 import os
 from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin
+from flask_security.models import fsqla_v3 as fsqla
+
 import secrets
 from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
+
+
 
 # Generate a secure key
 secure_key = secrets.token_urlsafe(32)  # 32 bytes for a reasonably long key
 
 # Generate a random 128-bit (16-byte) salt
-# salt = secrets.token_bytes(16)
+salt = secrets.token_bytes(16)
 
-salt = secrets.SystemRandom()  #secure random number generator
+# salt = secrets.SystemRandom()  #secure random number generator  
 
 # Never Print the secure key
 # print("Generated Secure Key:", secure_key,'\n',"Generated Salt:", salt)
@@ -23,23 +29,33 @@ app.config['SECURITY_PASSWORD_SALT'] = os.environ.get("SECURITY_PASSWORD_SALT", 
 app.config["REMEMBER_COOKIE_SAMESITE"] = "strict"
 app.config["SESSION_COOKIE_SAMESITE"] = "strict"
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:1234@localhost/flask_db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:1234@localhost/flask_database"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
     "pool_pre_ping": True,
 }
 
-db = SQLAlchemy(app)   
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
+fsqla.FsModels.set_db_info(db)
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
-    
+    fs_uniquifier = db.Column(db.String(64), nullable=False, default='101')
 
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+
+
+# Setup Flask-Security
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+app.security = Security(app, user_datastore)
 
 # Creating Tables
 with app.app_context():
@@ -53,7 +69,7 @@ def register():
         password = request.form.get('password')
 
         hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
-
+        print("Hashed Password:", hashed_password)
         new_user = User(username=username, email=email, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
@@ -73,6 +89,9 @@ def login():
         if user and bcrypt.check_password_hash(user.password, password):
             # Login successful
             return redirect(url_for('dashboard'))
+        else:
+    # Print or log the hashed password for debugging    
+         print("Stored Hashed Password:", user.password)
 
         # Login unsuccessful
         return render_template('login.html', error='Invalid username or password')
@@ -85,6 +104,11 @@ def dashboard():
     username = "username"  
     return render_template('dashboard.html', username=username)
 
-
 if __name__ == "__main__":
     app.run(debug=True, port='5005')
+
+
+# Commands for migrating 
+# flask --app securing_password db init 
+# flask --app securing_password db migrate
+# flask --app securing_password db upgrade
